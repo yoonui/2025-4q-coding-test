f28635c8c01951207eaafcbc4c5c9c4b732a12e4
yoonui <yoony0612@naver.com>
2025-11-15 13:15:29 +0900

    Lv.0 - 문자열 섞기

---
+++ 11월/251115_3_문자열_섞기.py
@@ -0,0 +1,10 @@
+def solution(str1, str2):
+    answer = ''
+
+    for i in range(0, len(str1)):
+        answer += str1[i]
+        answer += str2[i]
+        
+    return answer
+
+solution('aaaaa', 'bbbbb')
